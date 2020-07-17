42bf4f48309ec65a0a6028eb88ebbef2262d120e
przekwas <luke@covalence.io>
2020-07-17 11:29:16 -0500

    showing how to access a userid to insert

--- src/server/routes/api/lulz.ts
+++ src/server/routes/api/lulz.ts
@@ -1,11 +1,11 @@
 import * as passport from 'passport';
 import { Router } from 'express';
+import type { ReqUser } from '../../utils/types';
 
 const router = Router();
 
-router.get('/', passport.authenticate('jwt'), (req, res) => {
-	// req.user.id
-	res.json({ msg: 'This is some data!' });
+router.get('/', passport.authenticate('jwt'), (req: ReqUser, res) => {
+	res.json({ msg: 'This is some data!', userid: req.user.id });
 });
 
 export default router;
